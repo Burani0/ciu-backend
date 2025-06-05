2f91d45a34df4ece9f0a7e4739251fcfa5840db3
Winifred Nakawunde <winifrednakawunde07@gmail.com>
2025-06-05 09:45:13 +0300

    lecturer courses

---
+++ project/src/utils/api.tsx
@@ -0,0 +1,5 @@
+export const fetchLecturerWithCourses = async (lecturerId: string) => {
+    const res = await fetch(`http://localhost:3001/api/lecturers/${lecturerId}`);
+    if (!res.ok) throw new Error("Failed to fetch lecturer");
+    return res.json();
+  };
