8b3169b4cd34a7c1c7b022928a3bc8951807dbb1
Burani0 <buranipatiencepatricia@gmail.com>
2025-06-14 10:23:20 +0300

    updated the admin routes

--- project/src/components/CreateAdminPage.tsx
+++ project/src/components/CreateAdminPage.tsx
@@ -2,18 +2,28 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const CreateAdminPage = () => {
+  const [first_name, setFirst_name] = useState('');
+  const [last_name, setLast_name] = useState('');
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleCreate = async () => {
     try {
       await axios.post('http://localhost:3001/api/admin/create-admin', {
+        first_name,
+        last_name,
         username,
+        email,
         password,
       });
       alert('Admin created');
+      setFirst_name('');
+      setLast_name('');
       setUsername('');
+      setEmail('');
       setPassword('');
+
     } catch (err: any) {
       alert(err.response?.data?.message || 'Error creating admin');
     }
@@ -24,9 +34,29 @@ const CreateAdminPage = () => {
       <h1 className="text-xl font-bold mb-4">Create Admin</h1>
       <input
         className="w-full border p-2 mb-4"
+        placeholder="First name"
+        value={first_name }
+        onChange={(e) => setFirst_name(e.target.value)}
+      />
+      <input
+        className="w-full border p-2 mb-4"
+        placeholder="Lastname"
+        value={last_name}
+        onChange={(e) => setLast_name(e.target.value)}
+      />
+
+      <input
+        className="w-full border p-2 mb-4"
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        className="w-full border p-2 mb-4"
+        placeholder="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className="w-full border p-2 mb-4"
@@ -43,3 +73,4 @@ const CreateAdminPage = () => {
 };
 
 export default CreateAdminPage;
+
