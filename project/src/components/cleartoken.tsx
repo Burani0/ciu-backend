9847f81cd5d236567c5b7bce69274827a927943e
Burani0 <buranipatiencepatricia@gmail.com>
2025-06-16 07:12:31 +0300

    changing the routes to hosted version

--- project/src/components/cleartoken.tsx
+++ project/src/components/cleartoken.tsx
@@ -68,7 +68,7 @@ const Cleartoken = () => {
   const handleVerify = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3001/api/admin/cleartoken', {
+      await axios.post('http://ciu-backend.onrender.com/api/admin/cleartoken', {
         username,
         token,
       });
