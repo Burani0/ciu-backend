46632c6d6632ddcaaae589845ec4d73f48985d8b
Burani0 <patiencepatriciaburani>
2025-06-21 11:01:34 +0300

    allowed prefilght

--- project/server/index.js
+++ project/server/index.js
@@ -78,6 +78,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+app.options('*', cors());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
